9d2792405fb63075c007f2c183e9afb33b4d288b
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2026-06-07 21:05:16 -0400

    Testing launching an offline battle

--- src/renderer/shell/battle.ts
+++ src/renderer/shell/battle.ts
@@ -2,6 +2,11 @@
 //
 // SPDX-License-Identifier: MIT
 import { CommandModel } from "@renderer/shell/commandModel";
+import { outputError } from "@renderer/shell/error";
+import { battleActions } from "@renderer/store/battle.store";
+import { mapsStore } from "@renderer/store/maps.store";
+import { db } from "@renderer/store/db";
+import { battleStore } from "@renderer/store/battle.store";
 
 export const battleCommands: CommandModel = {
     help: ["Commands related to offline gameplay"],
@@ -12,5 +17,22 @@ export const battleCommands: CommandModel = {
         teams: {
             help: ["Set the allyteam configuration"],
         },
+        quick: {
+            help: ["Immediately launches a random battle."],
+            function: onQuickBattle,
+        },
     },
 };
+
+async function onQuickBattle() {
+    try {
+        const mapName = mapsStore.availableMapNames.values().next().value;
+        const map = await db.maps.get(mapName!);
+        console.log(mapName);
+        battleActions.resetToDefaultBattle(undefined, undefined, map, false);
+        battleStore.battleOptions.map = map;
+        battleActions.startBattle();
+    } catch (error) {
+        outputError(String(error));
+    }
+}
